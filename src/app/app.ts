1a2b32d3c7428240beb21a9082b3e50c31a64fd2
lucasmercier <lucas-mercier@hotmail.fr>
2025-09-28 21:26:46 +0200

    ui not responsive ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,13 @@ import { SecurityAndData } from "./security-and-data/security-and-data";
 import { Costs } from "./costs/costs";
 import { Reviews } from "./reviews/reviews";
 import { GetStarted } from "./get-started/get-started";
+import { Faq } from "./faq/faq";
+import { Contactform } from "./contactform/contactform";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Hero, Fonctionalities, SecurityAndData, Costs, Reviews, GetStarted],
+  imports: [Hero, Fonctionalities, SecurityAndData, Costs, Reviews, GetStarted, Faq, Contactform, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
