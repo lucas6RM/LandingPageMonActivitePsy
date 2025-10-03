32cdeb0857aa5a895dbc9ea3dbcb98c05cb29e05
lucasmercier <lucas-mercier@hotmail.fr>
2025-10-04 00:06:32 +0200

    link to docs

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,8 @@
+// app.routes.server.ts
+import { RenderMode, ServerRoute } from '@angular/ssr';
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Server,
+  },
+];
