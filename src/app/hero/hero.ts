341d3a11b22fbe2d01becadf01dd9345675e16b8
lucasmercier <lucas-mercier@hotmail.fr>
2025-10-04 11:44:58 +0200

    wip

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './hero.css'
 })
 export class Hero {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
